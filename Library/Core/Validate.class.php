e66253d8e281761e990180f97d55b760f1acfcab
Christopher Hill <cjhill@gmail.com>
2013-04-21 19:08:44 +0100

    Major changes to the Validate class, adding documentation, allowing the class to be run statically, and bug fixes.

--- Library/Core/Validate.class.php
+++ Library/Core/Validate.class.php
@@ -4,19 +4,88 @@ namespace Core;
 /**
  * Validates user input.
  *
+ * This class can be run in "single" mode or "mass" mode. In both situations it
+ * is designed to be human-readable, and provide a simple API to basic validation.
+ *
+ * Single mode example
+ * -------------------
+ *
+ * <code>
+ * if (Core\Validate::is('cjhill@gmail.com', 'email')) {
+ *     // Houston, we have a valid email address
+ * }
+ * </code>
+ *
+ * Mass mode example
+ * -----------------
+ *
+ * <code>
+ * $form = new Validate(array(
+ *     'Email address' => array(
+ *         'value' => 'cjhill@gmail.com',
+ *         'tests' => array(
+ *             'required' => true,
+ *             'is'       => 'email'
+ *         )
+ *     ),
+ *     'Age' => array(
+ *         'value' => 20,
+ *         'tests' => array(
+ *             'between' => array('min' => 13, 'max' => 99)
+ *         )
+ *     ),
+ *     'Password' => array(
+ *         'value' => 's3cr37',
+ *         'tests' => array(
+ *             'required'      => true,
+ *             'required_with' => array('Email address')
+ *             'length'        => array('min' => 8)
+ *         )
+ *     )
+ * ));
+ *
+ * if (! $form->isValid()) {
+ *     // Errors
+ *     var_dump($validate->getErrors());
+ * }
+ * </code>
+ *
  * @copyright   2012 Christopher Hill <cjhill@gmail.com>
  * @author      Christopher Hill <cjhill@gmail.com>
  * @since       21/04/2013
+ *
+ * @todo        Integrate with a Core\Notice class.
+ * @todo        Import data from a request and "bolt" on tests.
+ * @todo        Regex testing.
+ * @todo        Look into making the 'is' parameter class constants?
  */
 class Validate
 {
 	/**
 	 * The inputs that need to be validated.
 	 *
+	 * List of possible tests to perform:
+	 *
+	 * <ul>
+	 *     <li>'required'      => true</li>
+	 *     <li>'required_with' => array('foo', 'bar')</li>
+	 *     <li>
+	 *         You can explicitly set both min and max, or just one:
+	 *         <ul>
+	 *             <li>'length' => array('min' => 5, 'max' => 10)</li>
+	 *             <li>'length' => array('min' => 5)</li>
+	 *             <li>'length' => array('max' => 10)</li>
+	 *         </ul>
+	 *     </li>
+	 *     <li>'is'      => 'boolean|email|float|int|ip|url'</li>
+	 *     <li>'exactly' => array('foobar', 'acme')/li>
+	 *     <li>'between' => array('min' => 10, 'max' => 100)</li>
+	 * </ul>
+	 *
 	 * @access private
 	 * @var    array
 	 */
-	private $_input     = array();
+	private $_input = array();
 
 	/**
 	 * Whether we have run the validation tests on the inputs.
@@ -32,7 +101,7 @@ class Validate
 	 * @access private
 	 * @var    array
 	 */
-	private $_error     = array();
+	private $_error = array();
 
 	/**
 	 * Set the inputs that need to be validated.
@@ -65,7 +134,7 @@ class Validate
 
 		// Loop through each input
 		foreach ($this->_input as $inputName => $inputInfo) {
-			// Sanity check: We have tests to perform
+			// Sanity check that we have tests to perform
 			if (! isset($inputInfo['tests'])) {
 				continue;
 			}
@@ -74,7 +143,7 @@ class Validate
 			foreach ($inputInfo['tests'] as $testName => $testParams) {
 				// Run the test
 				// Note: The function to test might not exist, so we can catch with __call.
-				if (! $this->$testName($inputInfo['value'], (array)$testParams)) {
+				if (! self::$testName($inputInfo['value'], (array)$testParams)) {
 					// The test was unsuccessful, report error
 					$this->addError($inputName, $testName);
 				}
@@ -85,7 +154,7 @@ class Validate
 		$this->_validated = true;
 
 		// Pass back whether it was successful or not
-		return $this->getIsValid();
+		return $this->isValid();
 	}
 
 	/**
@@ -102,6 +171,9 @@ class Validate
 
 	/**
 	 * Test to see if the input's other required fields are valid and exist.
+	 *
+	 * We only test to see if the required inputs have been defined. Any further
+	 * validating should be done on the required input.
 	 *
 	 * @access private
 	 * @param  string  $inputValue The value of the input.
@@ -130,19 +202,24 @@ class Validate
 	 *
 	 * Note: This function can deal with a min, a max, or a min and a max boundary.
 	 *
-	 * @access private
-	 * @param  string  $inputValue The value of the input.
-	 * @param  array   $testParams The parameters for this test.
+	 * @access public
+	 * @param  string|array $inputValue The value of the input.
+	 * @param  array        $testParams The parameters for this test.
 	 * @return boolean
+	 * @static
+	 * @throws \Exception   If neither a min or max boundary is specified.
 	 */
-	private function length($inputValue, $testParams) {
+	public static function length($inputValue, $testParams) {
 		// We need to have at least one boundary
 		if (! isset($testParams['min']) && ! isset($testParams['max'])) {
-			throw new Exception('Length test requires at least a min or max boundary.');
+			throw new \Exception('Length test requires at least a min or max boundary.');
 		}
 
 		// Get the string length of the inputs value
-		$length = strlen($inputValue);
+		// Note: We could have been given a string or array
+		$length = is_array($inputValue)
+			? count($inputValue)
+			: strlen($inputValue);
 
 		// Length is less than the minimum
 		if (isset($testParams['min']) && $length < $testParams['min']) {
@@ -160,12 +237,13 @@ class Validate
 	/**
 	 * Test to see if the input is a specific type.
 	 *
-	 * @access private
+	 * @access public
 	 * @param  string  $inputValue The value of the input.
 	 * @param  array   $testParams The parameters for this test.
 	 * @return boolean
+	 * @static
 	 */
-	private function is($inputValue, $testParams) {
+	public static function is($inputValue, $testParams) {
 		// Work out which filter we should use
 		switch ($testParams) {
 			case 'boolean' : $filter = \FILTER_VALIDATE_BOOLEAN; break;
@@ -184,15 +262,15 @@ class Validate
 	/**
 	 * Test to see if the input matches exactly another value.
 	 *
-	 * Note: This test also checks for the same data type, so "1" and 1 will
-	 * return false because string and int are not the same datatype.
+	 * Note: We also check for same data types, so "1" and 1 will fail.
 	 *
-	 * @access private
+	 * @access public
 	 * @param  string  $inputValue The value of the input.
 	 * @param  array   $testParams The parameters for this test.
 	 * @return boolean
+	 * @static
 	 */
-	private function exactly($inputValue, $testParams) {
+	public static function exactly($inputValue, $testParams) {
 		return in_array($inputValue, $testParams, true);
 	}
 
@@ -201,20 +279,21 @@ class Validate
 	 *
 	 * Note: We check the datatype of the input first to make sure it is an int.
 	 *
-	 * @access private
+	 * @access public
 	 * @param  string     $inputValue The value of the input.
 	 * @param  array      $testParams The parameters for this test.
 	 * @return boolean
+	 * @static
 	 * @thorws \Exception If the user has supplied no min and max boundary.
 	 */
-	private function between($inputValue, $testParams) {
+	public static function between($inputValue, $testParams) {
 		// User has supplied both boundaries?
 		if (! isset($testParams['min']) || ! isset($testParams['max'])) {
 			throw new \Exception('Between expects a min and max boundary.');
 		}
 
 		// Make sure the user has given us an int
-		else if (! $this->is($inputValue, 'int')) {
+		else if (! self::is($inputValue, 'int')) {
 			return false;
 		}
 
@@ -256,7 +335,7 @@ class Validate
 	 * @access public
 	 * @return boolean
 	 */
-	public function getIsValid() {
+	public function isValid() {
 		return count($this->_error) >= 1;
 	}
 
@@ -287,9 +366,10 @@ class Validate
 	 * @access public
 	 * @param  string     $functionName The name of the method the user called.
 	 * @param  array      $params       The parameters that the user passed in.
-	 * @thorws \Exception
+	 * @magic
+	 * @throws \Exception If called then the user has entered an invalid test name.
 	 */
-	public function __call($functionName, $params) {
-		throw new \Exception($functionName . ' is not a valid validating method.');
+	public function __call($testName, $params) {
+		throw new \Exception($testName . ' is not a valid test.');
 	}
 }
