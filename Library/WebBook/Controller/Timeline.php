eb936866b2f8209e5f1a36558cff00eb8a96c075
Christopher Hill <cjhill@gmail.com>
2013-09-05 22:31:04 +0100

    Setting up the barebones for the timeline page.

---
+++ Library/WebBook/Controller/Timeline.php
@@ -0,0 +1,21 @@
+<?php
+namespace WebBook\Controller;
+use Core, WebBook\Model;
+
+/**
+ * Handles the interaction with the timeline page..
+ *
+ * @copyright 2013 Christopher Hill <cjhill@gmail.com>
+ * @author    Christopher Hill <cjhill@gmail.com>
+ */
+class Timeline extends Core\Controller
+{
+	/**
+	 * Displays the overview page.
+	 *
+	 * @access public
+	 */
+	public function indexAction() {
+		// Do nothing
+	}
+}
